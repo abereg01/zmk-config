acfd922334e8bd3e6267f554fdec1d1b4d2550b1
abereg01 <abereg01@pm.me>
2025-07-25 20:30:10 +0200

    TOTEM

--- rules.mk
+++
@@ -1,4 +0,0 @@
-# Notes - Om du vill ta bort auto shift för att spela, välj no & compilea ny config qmk compile -kb kbdfans/maja -km andreas_maja
-VIA_ENABLE = yes
-LTO_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
